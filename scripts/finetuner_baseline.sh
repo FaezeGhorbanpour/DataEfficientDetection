99627e651d07aa6a42a1a3d7cbe56b45c3087357
Faeze <faezeghorbanpour96@gmail.com>
2024-12-23 21:50:35 +0100

    baseline script fixed

--- scripts/finetuner_baseline.sh
+++ scripts/finetuner_baseline.sh
@@ -4,19 +4,20 @@ BASE="/mounts/work/faeze/data_efficient_hate"
 # Configuration
 DATASETS=('bas19_es' 'for19_pt' 'has21_hi' 'ous19_ar' 'ous19_fr' 'san20_it')
 LANGUAGES=('es' 'pt' 'hi' 'ar' 'fr' 'it')
+RSS=(rs1 rs2 rs3 rs4 rs5)
 SEEDS=(42 30 0 100 127)
 
 MODEL_NAME="FacebookAI/xlm-roberta-base"
 
 for dataset in "${DATASETS[@]}"; do
     for split in 10 20 30 40 50 100 200 300 400 500 1000 2000; do
-        for rs in rs1 rs2 rs3 rs4 rs5; do
-            OUTPUT_DIR = "${BASE}/models/finetuner/roberta-default/${dataset}-${split}/${rs}/"
+        for ((i=0; i<${#RSS[@]}; i++)); do
+            OUTPUT_DIR="${BASE}/models/finetuner/roberta-default/${dataset}-${split}/${RSS[i]}/"
             python main.py \
                 --finetuner_model_name_or_path "${MODEL_NAME}" \
-                --datasets "[\"${dataset}-${split}-${rs}\"]" \
+                --datasets "[\"${dataset}-${split}-${RSS[i]}\"]" \
                 --languages "[\"${LANGUAGES[@]}\"]" \
-                --seed "${SEEDS[@]}" \
+                --seed "${SEEDS[i]}" \
                 --do_fine_tuning \
                 --do_train \
                 --do_test \
@@ -24,7 +25,7 @@ for dataset in "${DATASETS[@]}"; do
                 --per_device_eval_batch_size 64 \
                 --num_train_epochs 5 \
                 --max_seq_length 128 \
-                --output_dir ${OUTPUT_DIR} \
+                --output_dir $OUTPUT_DIR \
                 --cache_dir "${BASE}/cache/" \
                 --logging_dir "${BASE}/logs/" \
                 --overwrite_output_dir \
