251c033e80091a4d3ba73b7baf833d5642b859cc
Faeze <faezeghorbanpour96@gmail.com>
2024-12-23 22:00:34 +0100

    scrips are updated

--- scripts/finetuner_baseline_en.sh
+++ scripts/finetuner_baseline_en.sh
@@ -4,19 +4,20 @@ BASE="/mounts/work/faeze/data_efficient_hate"
 # Configuration
 DATASETS=('dyn21_en' 'fou18_en' 'ken20_en')
 LANGUAGES=('en' 'en' 'en')
+RSS=(rs1 rs2 rs3 rs4 rs5)
 SEEDS=(42 30 0 100 127)
 
 MODEL_NAME="FacebookAI/xlm-roberta-base"
 
 for dataset in "${DATASETS[@]}"; do
     for split in 20000; do
-        for rs in rs1 rs2 rs3 rs4 rs5; do
-            OUTPUT_DIR = "${BASE}/models/finetuner/roberta-default/${dataset}-${split}/${rs}/"
+        for ((i=0; i<${#RSS[@]}; i++)); do
+            OUTPUT_DIR="${BASE}/models/finetuner/roberta-default/${dataset}-${split}/${RSS[i]}/"
             python main.py \
                 --finetuner_model_name_or_path "${MODEL_NAME}" \
-                --datasets "[\"${dataset}-${split}-${rs}\"]" \
-                --languages "[\"${LANGUAGES[@]}\"]" \
-                --seed "${SEEDS[@]}" \
+                --datasets "${dataset}-${split}-${RSS[i]}" \
+                --languages "${LANGUAGES[@]}" \
+                --seed "${SEEDS[i]}" \
                 --do_fine_tuning \
                 --do_train \
                 --do_test \
@@ -24,11 +25,11 @@ for dataset in "${DATASETS[@]}"; do
                 --per_device_eval_batch_size 64 \
                 --num_train_epochs 5 \
                 --max_seq_length 128 \
-                --output_dir ${OUTPUT_DIR} \
+                --output_dir $OUTPUT_DIR \
                 --cache_dir "${BASE}/cache/" \
                 --logging_dir "${BASE}/logs/" \
                 --overwrite_output_dir \
-                --wandb_run_name "fine_tuning_baseline"
+                --wandb_run_name "fine_tuning_mono"
 
             # Clean up checkpoint files
             rm -rf "${OUTPUT_DIR}/check*"
--- scripts/finetuner_baseline_multi-threading.sh
+++ scripts/finetuner_baseline_multi-threading.sh
@@ -5,6 +5,7 @@ BASE="/mounts/work/faeze/data_efficient_hate"
 # Configuration
 DATASETS=('bas19_es' 'for19_pt' 'has21_hi' 'ous19_ar' 'ous19_fr' 'san20_it')
 LANGUAGES=('es' 'pt' 'hi' 'ar' 'fr' 'it')
+RSS=(rs1 rs2 rs3 rs4 rs5)
 SEEDS=(42 30 0 100 127)
 
 MODEL_NAME="FacebookAI/xlm-roberta-base"
@@ -21,26 +22,25 @@ run_dataset() {
     local gpu_id=$2
 
     for split in 10 20 30 40 50 100 200 300 400 500 1000 2000; do
-        for rs in rs1 rs2 rs3 rs4 rs5; do
-            OUTPUT_DIR="${BASE}/models/finetuner/roberta-default/${dataset}-${split}/${rs}/"
-            echo "Running dataset ${dataset} with split ${split} on GPU ${gpu_id}"
-            CUDA_VISIBLE_DEVICES=$gpu_id python main.py \
+        for ((i=0; i<${#RSS[@]}; i++)); do
+            OUTPUT_DIR="${BASE}/models/finetuner/roberta-default/${dataset}-${split}/${RSS[i]}/"
+            python main.py \
                 --finetuner_model_name_or_path "${MODEL_NAME}" \
-                --datasets "[\"${dataset}-${split}-${rs}\"]" \
-                --languages "[\"${LANGUAGES[@]}\"]" \
-                --seed "${SEEDS[@]}" \
+                --datasets "${dataset}-${split}-${RSS[i]}" \
+                --languages "${LANGUAGES[@]}" \
+                --seed "${SEEDS[i]}" \
                 --do_fine_tuning \
                 --do_train \
                 --do_test \
                 --per_device_train_batch_size 16 \
                 --per_device_eval_batch_size 64 \
-                --num_train_epochs 5 \
+                --num_train_epochs 10 \
                 --max_seq_length 128 \
-                --output_dir "${OUTPUT_DIR}" \
+                --output_dir $OUTPUT_DIR \
                 --cache_dir "${BASE}/cache/" \
                 --logging_dir "${BASE}/logs/" \
                 --overwrite_output_dir \
-                --wandb_run_name "fine_tuning_baseline"
+                --wandb_run_name "fine_tuning_mono"
 
             # Clean up checkpoint files
             rm -rf "${OUTPUT_DIR}/check*"
