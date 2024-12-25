782872567edfbe9e34e92d20660baa04dda6b24b
Faeze <faezeghorbanpour96@gmail.com>
2024-12-25 15:58:57 +0100

    nothing

--- scripts/finetuner_rottger_multi-threading.sh
+++ scripts/finetuner_rottger_multi-threading.sh
@@ -4,14 +4,14 @@ BASE="/mounts/work/faeze/data_efficient_hate"
 # Configuration
 DATASETS=('bas19_es' 'for19_pt' 'has21_hi' 'ous19_ar' 'ous19_fr' 'san20_it' 'xdomain_tr' 'gahd24_de')
 LANGUAGES=('es' 'pt' 'hi' 'ar' 'fr' 'it' 'tr' 'de')
-SEEDS=(42 30 0 100 127)
 RSS=(rs1 rs2 rs3 rs4 rs5)
 GPUS=(0 1 2 3 4 5 6 7) # Adjust based on available GPUs
 
-#TOKENIZER_NAME="cardiffnlp/twitter-xlm-roberta-base"
-TOKENIZER_NAME="microsoft/mdeberta-v3-base"
-MODEL_NAME="/mounts/work/faeze/data_efficient_hate/models/finetuner/mdeberta-default/ken20_en-20000/rs4/checkpoint-2500"
-
+TOKENIZER_NAME="cardiffnlp/twitter-xlm-roberta-base"
+#TOKENIZER_NAME="microsoft/mdeberta-v3-base"
+MODEL_NAME="/mounts/work/faeze/data_efficient_hate/models/finetuner/twitter-roberta-default/dyn21_en-20000/rs4/checkpoint-5000"
+FOLDER_NAME="twitter-roberta"
+FOLDER_SUBNAME="dyn21"
 # Function to process a single dataset
 run_dataset() {
     local dataset=$1
@@ -22,13 +22,13 @@ run_dataset() {
 
     for split in 10 20 30 40 50 100 200 300 400 500 1000 2000; do
         for ((i=0; i<${#RSS[@]}; i++)); do
-            OUTPUT_DIR="${BASE}/models/finetuner/mdeberta-ken20/${dataset}-${split}/${RSS[i]}/"
+            OUTPUT_DIR="${BASE}/models/finetuner/${FOLDER_NAME}-${FOLDER_SUBNAME}/${dataset}-${split}/${RSS[i]}/"
             CUDA_VISIBLE_DEVICES=${gpu} python main.py \
                 --finetuner_model_name_or_path "${MODEL_NAME}" \
 		--finetuner_tokenizer_name_or_path "${TOKENIZER_NAME}" \
                 --datasets "${dataset}-${split}-${RSS[i]}" \
                 --languages "${lang}" \
-                --seed "${SEEDS[i]}" \
+                --seed ${RSS[i]//rs/} \
                 --do_fine_tuning \
                 --do_train \
                 --do_test \
@@ -40,10 +40,10 @@ run_dataset() {
                 --cache_dir "${BASE}/cache/" \
                 --logging_dir "${BASE}/logs/" \
                 --overwrite_output_dir \
-                --wandb_run_name "fine_tuning_mdeberta_ken20"
+                --wandb_run_name "fine_tuning_${FOLDER_SUBNAME}"
 
             # Clean up checkpoint files
-            rm -rf "${OUTPUT_DIR}/check*"
+            rm -rf "${OUTPUT_DIR}checkpoint*"
         done
     done
 
